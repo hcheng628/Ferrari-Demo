853bee47bda59044875c69cec13eeeb4a4084519
superhcheng <chenghongyu628@gmail.com>
2017-08-07 16:14:49 -0400

    complete startapp + getappinfo; adding dependencies for xml2json, angularmodal

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', [
+    'ngRoute',
+    'mgcrea.ngStrap',
+    'ngAnimate'
+    ])
 
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {
@@ -9,7 +13,7 @@ angular.module('myApp.view1', ['ngRoute'])
         });
     }])
 
-    .controller('RegisterCtrl', [ '$scope', '$location', 'CFService', function($scope, $location, CFService) {
+    .controller('RegisterCtrl', [ '$scope', '$location', '$modal', 'CFService', function($scope, $location, $modal, CFService) {
 
         $scope.cfService = CFService;
         $scope.steps = [
@@ -20,9 +24,23 @@ angular.module('myApp.view1', ['ngRoute'])
             'Step 5: Agreement & Verification'
         ];
 
+
+        $scope.showCreateModal = function () {
+            $scope.createModal = $modal({
+                scope: $scope,
+                template: 'view1/templates/modal.offers.tpl.html',
+                show: true
+            })
+        };
+
+
         $scope.startApp = function () {
             $scope.cfService.startApp();
         };
+
+        $scope.getAppInfo =function () {
+            $scope.cfService.getAppInfo();
+        }
 
 
         $scope.selection = $scope.steps[0];
@@ -75,46 +93,43 @@ angular.module('myApp.view1', ['ngRoute'])
 
     .service('CFService', function ($http) {
         var self = {
-            'firstName': "",
-            'lastName': "",
-            'email': '',
-            'ssn':'',
-            'birthdate': '',
-            'phone':'',
-            'address': '',
+            'processEngineGuid': '05d1ba20-7b8f-11e7-80eb-3e8b20524153',
+            'firstName': "JONATHAN",
+            'lastName': "CONSUMER",
+            'email': 'praful.asher.uat@gmail.com',
+            'ssn':999999991,
+            'birthdate': '1989-12-17',
+            'phone':'703650066',
+            'address': '630 BASKINS RD',
             'address2':'',
-            'city': '',
-            'state':'',
-            'zip':'',
-            'houseType':'',
-            'residenceSince': '',
-            'mortgageRent':'',
-            'bankruptcy':'',
-            'employmentStatus':'',
-            'employer':'',
-            'employerSince':'',
-            'monthlyIncome':'',
-            'otherIncome':'',
-            'monthlyExpenses':'',
-            'loanType':'',
-            'loanAmount': '',
+            'city': 'BURLISON',
+            'state':'CA',
+            'zip':'91325',
+            'houseType':'1',
+            'residenceSince': '2006-12-17',
+            'mortgageRent':1200,
+            'bankruptcy':'N',
+            'employmentStatus':'1',
+            'employer':'ACCB',
+            'employerSince':'2000-12-17',
+            'monthlyIncome':7500,
+            'otherIncome':1000,
+            'monthlyExpenses':2000,
+            'loanType':'AUTO',
+            'loanAmount': 50000,
             'externalAppId': '',
+            'offers': [],
             'isSearching': false,
             'isSaving': false,
-            'selectedPerson': null,
-            'imagefile': null,
-            'selfiefile': null,
-            'selfieStr': '',
-            'persons': [],
             'search': null,
             'startApp': function () {
                 var endDate = new Date();
                 var startResdate = new Date(self.residenceSince);
                 var startEmpDate = new Date(self.employerSince);
 
-                var numOfResMonths = endDate.getMonth() - startResdate.getMonth() + (12 * (endDate.getFullYear() - startResdate.getFullYear())) % 12;
+                var numOfResMonths = parseInt(endDate.getMonth() - startResdate.getMonth() + (12 * (endDate.getFullYear() - startResdate.getFullYear()))) % 12;
                 var numOfResYears = Math.floor(endDate.getMonth() - startResdate.getMonth() + (12 * (endDate.getFullYear() - startResdate.getFullYear())) / 12);
-                var numOfEmpMonths = endDate.getMonth() - startEmpDate.getMonth() + (12 * (endDate.getFullYear() - startEmpDate.getFullYear())) % 12;
+                var numOfEmpMonths = parseInt(endDate.getMonth() - startEmpDate.getMonth() + (12 * (endDate.getFullYear() - startEmpDate.getFullYear()))) % 12;
                 var numOfEmpYears = Math.floor(endDate.getMonth() - startEmpDate.getMonth() + (12 * (endDate.getFullYear() - startEmpDate.getFullYear())) / 12);
                 self.externalAppId = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) + endDate.getTime();
 
@@ -139,20 +154,6 @@ angular.module('myApp.view1', ['ngRoute'])
                     + " Email='" + self.email + "' PhoneNumber='" + self.phone.substring(3,10) + "'/></Applicant></DocumentInput>]]></documentInput>"
                     + "</cfp:startApplication></soapenv:Body></soapenv:Envelope>";
 
-                /*
-                 var msgTest = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:cfp='http://xmlns.crif.com/schema/CFProxy'>"
-                 + "<soapenv:Header/><soapenv:Body><cfp:startApplication><username>ApplicationStarter</username><password>password</password><processCacheId>MOVECU_COP</processCacheId>"
-                 + "<processVersion></processVersion><documentInput><![CDATA[<DocumentInput><Header DateTimeCreated='2016-09-12T18:39:08+05:30'"
-                 + " OpenSkyApplicationID='" + "cheng123456" + "'/><Application><Product LoanAmountRequest='" + "1200" +  "' LoanType='AUTO' Term='60'>"
-                 + "<Vehicle CbCondition='N' CurrentMileage='0' Downpayment='0' ModelYear='2016' Price='30000.00' Type='AUTO' /></Product></Application>"
-                 + "<Applicant ActiveDutyMilitary='N' ApplicantID='1' Bankruptcy='"  + "N" + "' Birthdate='" + "1989-12-17" + "' CoSignerAvailable='N'"
-                 + " FirstName='" + "JOHN" + "' LastName='" + "CONSUMER" + "' MiddleName='' MonthlyDebt='" + "1200" + "' MonthlyExpenses='"+ "2000" +"'"
-                 + " MonthlyIncome='" + "7500" + "' Role='B' SSN='"+ "726554928" +"' Salutation='' Type='I'><Employment EmploymentStatus='1' JobTitle='Nothing' Name='" + "CRIF" + "'"
-                 + " TimeAtWorkMonths='5' TimeAtWorkYears='5' /><Address City='"+ "Newark" +"' HousingStatus='" + "1" + "' Line1='"+ "630 BASKINS RD" +"' Line2='"+ "" +"'"
-                 + " MonthlyMortgage_Rent='"+ "1200" +"' PostalCode='" + "32857" + "' ResidenceMonths='10' ResidenceYears='10' State='"+ "GA" +"'/><Contact AreaCode='"+ "321" +"'"
-                 + " Email='" + "abc@abc.com" + "' PhoneNumber='" + "2265533" + "'/></Applicant></DocumentInput>]]></documentInput>"
-                 + "</cfp:startApplication></soapenv:Body></soapenv:Envelope>";
-                 */
                 // console.log(msg);
                 $http.post('http://10.110.28.172:8080/CFProxy-1.0.0/jaxws/CFProxyWS', msg, {
                     headers: {
@@ -165,7 +166,12 @@ angular.module('myApp.view1', ['ngRoute'])
                         'SOAPAction': ''
                     }
                 }).then(function(response){
-                    console.log(response);
+                    // console.log(response.data);
+                    var parser = new DOMParser();
+                    var xmlDoc = parser.parseFromString(response.data, "text/xml");
+                    if(xmlDoc.getElementsByTagName('ProcessEngineGuid')[0].childNodes[0].nodeValue) {
+                        self.processEngineGuid = xmlDoc.getElementsByTagName('ProcessEngineGuid')[0].childNodes[0].nodeValue;
+                    }
                 }, function(error){
                     console.err(error);
                 })
@@ -175,7 +181,47 @@ angular.module('myApp.view1', ['ngRoute'])
 
             },
             'getAppInfo': function () {
+                var msg = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:cfp='http://xmlns.crif.com/schema/CFProxy'><soapenv:Header/>"
+                + "<soapenv:Body><cfp:getApplicationInfo><username>ApplicationStarter</username><password>password</password><processEngineGuid>"
+                + self.processEngineGuid + "</processEngineGuid><applicationId>2903</applicationId></cfp:getApplicationInfo></soapenv:Body></soapenv:Envelope>";
 
+                $http.post('http://10.110.28.172:8080/CFProxy-1.0.0/jaxws/CFProxyWS', msg, {
+                    headers: {
+                        'Content-Type':'text/xml',
+                        'Accept':'text/xml',
+                        'Access-Control-Allow-Credentials':'true',
+                        'Access-Control-Allow-Methods':'POST',
+                        'Access-Control-Allow-Origin':'*',
+                        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Accept, X-Requested-With',
+                        'SOAPAction': ''
+                    }
+                }).then(function(response){
+                    // console.log(response.data);
+                    var parser = new DOMParser();
+                    var xmlDoc = parser.parseFromString(response.data, "text/xml");
+                    if(xmlDoc.getElementsByTagName('Offer').length > 0) {
+                        // self.offers = xmlDoc.getElementsByTagName('Offer');
+
+                    }
+                    var x2js = new X2JS();
+                    var aftCnv = x2js.xml_str2json(response.data);
+                    // console.log(aftCnv.Envelope.Body.getApplicationInfoResponse.return.Application._Status);
+                    // console.log(aftCnv.Envelope.Body.getApplicationInfoResponse.return.Offer);
+
+                    // console.log(aftCnv.Envelope.Body.getApplicationInfoResponse.return.Offer.length);
+
+                    for (var i=0; i < aftCnv.Envelope.Body.getApplicationInfoResponse.return.Offer.length; i++) {
+                        if(aftCnv.Envelope.Body.getApplicationInfoResponse.return.Offer[i].Id){
+                            self.offers.push(aftCnv.Envelope.Body.getApplicationInfoResponse.return.Offer[i]);
+                        }
+                    }
+
+                    console.log(self.offers);
+
+
+                }, function(error){
+                    console.err(error);
+                })
             }
         };
         return self;
