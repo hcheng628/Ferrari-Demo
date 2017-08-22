f09f6ab11c6ed2e4b49291e4ae0391c33e7b01b7
superhcheng <chenghongyu628@gmail.com>
2017-08-22 12:04:24 -0400

    typo fix

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -7,7 +7,8 @@ angular.module('myApp.view1', [
     'toaster',
     'angularSpinner',
     'angular-ladda',
-    'jcs-autoValidate'
+    'jcs-autoValidate',
+    'ui.mask'
 ])
     .run([
         'bootstrap3ElementModifier',
@@ -15,7 +16,7 @@ angular.module('myApp.view1', [
             bootstrap3ElementModifier.enableValidationStateIcons(true);
         }])
 
-    .config(['$routeProvider', 'laddaProvider', function($routeProvider, laddaProvider) {
+    .config(['$routeProvider', 'laddaProvider', function ($routeProvider, laddaProvider) {
 
         laddaProvider.setOption({
             style: 'expand-right'
@@ -26,8 +27,16 @@ angular.module('myApp.view1', [
             controller: 'RegisterCtrl'
         });
     }])
-    .controller('RegisterCtrl', [ '$scope', '$location', '$modal', 'usSpinnerService', 'CFService', function($scope, $location, $modal, usSpinnerService, CFService) {
+    .controller('RegisterCtrl', ['$scope', '$location', '$modal', 'usSpinnerService', 'CFService', function ($scope, $location, $modal, usSpinnerService, CFService) {
         $scope.cfService = CFService;
+
+        $scope.$watch('app_ssn', function(val, oldVal) {
+            console.log('app_ssn= \'' + val + '\'');
+        });
+
+        $scope.$watch('testForm.$valid', function(val, oldVal) {
+            console.log('$watch testForm.$valid ' + val);
+        });
 
         $scope.app_initPurAmt = null;
         $scope.app_income = null;
@@ -55,8 +64,6 @@ angular.module('myApp.view1', [
         $scope.autoPay_cardnum = null;
         $scope.autoPay_expiration = null;
         $scope.autoPay_securityCode = null;
-
-
 
 
         $scope.showBureauModal = function () {
@@ -95,6 +102,49 @@ angular.module('myApp.view1', [
         };
     })
 
+    .directive('validateSsn', function () {
+        var SSN_REGEXP = /^(?!000)(?!666)(?!9)\d{3}[- ]?(?!00)\d{2}[- ]?(?!0000)\d{4}$/;
+        var ssnPattern = {
+            3: '-',
+            5: '-'
+        };
+        return {
+            require: 'ngModel',
+            link: function (scope, elem, attr, ctrl) {
+                var formatSSN = function () {
+                    console.log("Chengcheng");
+                    var sTempString = ctrl.$viewValue;
+                    sTempString = sTempString.replace(/\-/g, '');
+                    var numbers = sTempString;
+                    var temp = '';
+                    for (var i = 0; i < numbers.length; i++) {
+                        temp += (ssnPattern[i] || '') + numbers[i];
+                    }
+                    ctrl.$viewValue = temp;
+
+                    scope.$apply(function () {
+                        elem.val(ctrl.$viewValue);
+                    });
+
+                };
+                ctrl.$parsers.unshift(function (viewValue) {
+                    // test and set the validity after update.
+                    var valid = SSN_REGEXP.test(viewValue);
+                    ctrl.$setValidity('ssnValid', valid);
+                    return viewValue;
+                });
+                // This runs when we update the text field
+                ctrl.$parsers.push(function (viewValue) {
+
+                    var valid = SSN_REGEXP.test(viewValue);
+                    ctrl.$setValidity('ssnValid', valid);
+                    return viewValue;
+                });
+                elem.bind('blur', formatSSN);
+
+            }
+        };
+    })
     .service('CFService', function ($http, toaster, $q) {
         var self = {
             'step1Complete': null,
